5576bfe5d18c75348ac1f72c667635eae29c96dc
Rachelle Gaudreau <rgaudreau@cs.binghamton.edu>
2024-11-01 18:28:10 -0400

    Print Matrix Update

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@ CC = gcc
 CFLAGS = -Wall -std=c18 -ggdb
 
 test : matrix
-	./matrix 3 3 <testEntriesMedium.txt
+	./matrix 2 2 1 2 3 4 1 2 3 4
+	./matrix 3 3 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9
+	./matrix 5 5 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
 
 matrix : matrix.c
 	$(CC) $(CFLAGS) -o matrix matrix.c
--- matrix.c
+++ matrix.c
@@ -2,59 +2,91 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
+    if(argc<3){
+        printf("How to use: <rows> <columns> <matrix_entries> \n");
+        return 1;
+    }
+
     int row = atoi(argv[1]);
     int col = atoi(argv[2]);
+    int totEntries = argc-3;
+    int numPerMatrix = row*col;
 
-    int totEntries = argc-2;
-    int numPerMatrix = row*col/2;
-
-    if(totEntries <=0){
+    /*
+    if(totEntries < numPerMatrix*2){
         printf("Make sure to include your matrix numbers...\n");
+        printf("Not enough entries for two matrices.\n");
+        return 1;
+    }
+    */
+
+    int **matrixOne = (int**)malloc(sizeof(int*)*row);
+    int **matrixTwo = (int**)malloc(sizeof(int*)*row);
+    
+    for(int i=0; i<row; i++){
+        matrixOne[i]=(int*)malloc(sizeof(int)*col);
+        matrixTwo[i]=(int*)malloc(sizeof(int)*col);
     }
 
-    int **matrixOne = (int**)malloc(sizeof(int)*row);
-    int **matrixTwo = (int**)malloc(sizeof(int)*numPerMatrix);
-    int in = 0;
+    printf("Rows: %d \n", row);
+    printf("Columns: %d \n", col);
+    int index = 3;
 
-    for(int j = 0; j<=numPerMatrix; j++){
-        for(int i = 0; i<=numPerMatrix; i++){
-            in = scanf(" %d", &in);
-            matrixOne[j][i]= in;
-            
-            //    char buf[100];
-            //    scanf(" %s", buf);
-            //    printf("Input %s not recognised... Quitting\n", buf);
-            //    break;
+
+    for(int j = 0; j< row; j++){
+        for(int i = 0; i< col; i++){
+            if(argv[index]!= NULL){
+                matrixOne[j][i]= atoi(argv[index]);
+                index++;
+            } else {
+                matrixOne[j][i]=0;
+            }
+        }
+    }
+    //index++;
+
+    // take in entries
+    for(int j = 0; j< row; j++){
+        for(int i = 0; i< col; i++){
+            if(argv[index]!= NULL){
+                matrixTwo[j][i]=atoi(argv[index]);
+                index++;
+            } else {
+                matrixTwo[j][i]=0;
+            }
         }
     }
 
-    in=0;
-    for(int j = numPerMatrix; j<=totEntries; j++){
-        for(int i = numPerMatrix; i<=totEntries; i++){
-            in = scanf(" %d", &in);
-            matrixTwo[j-numPerMatrix][i-numPerMatrix]=in;
-
-            //    char buf[100];
-            //    scanf(" %s", buf);
-            //    printf("Input %s not recognised... Quitting\n", buf);
-            //    break;
-        }
-    }
-
-    //repeat matrix1 in nice set up
+    // print matrix 1
     printf("Matrix 1: \n");
-    for(int j=0; j<=numPerMatrix; j++){
-        for(int i=0; i<=numPerMatrix; i++){
+    for(int j=0; j< row; j++){
+        for(int i=0; i< col; i++){
             printf("%d ", matrixOne[j][i]);
         }
         printf("\n");
     }
+
+    // print matrix 2
+    printf("Matrix 2: \n");
+    for(int j=0; j< row; j++){
+        for(int i=0; i< col; i++){
+            printf("%d ", matrixTwo[j][i]);
+        }
+        printf("\n");
+    }    
 
     //add 
     
     //subtract 
 
     //multiply
+
+    for(int i=0; i<row; i++){
+        free(matrixOne[i]);
+        free(matrixTwo[i]);
+    }
+    free(matrixOne);
+    free(matrixTwo);
 
     return 0;
 }
