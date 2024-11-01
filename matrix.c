0b22325f87597898858eda810542e2c1ec344924
Rachelle Gaudreau <107381065+rachellegaudreau@users.noreply.github.com>
2024-11-01 18:50:46 -0400

    Readability Update
    For quality of life I made code look neater and added comments for better understanding. I also uncommented and changed the check for enough entries.

--- matrix.c
+++ matrix.c
@@ -9,33 +9,30 @@ int main(int argc, char **argv){
 
     int row = atoi(argv[1]);
     int col = atoi(argv[2]);
-    int totEntries = argc-3;
+    int totEntries = argc-3; 
     int numPerMatrix = row*col;
 
-    /*
-    if(totEntries < numPerMatrix*2){
-        printf("Make sure to include your matrix numbers...\n");
-        printf("Not enough entries for two matrices.\n");
-        return 1;
+    if(totEntries < numPerMatrix * 2){
+        printf("Not enough entries for two matrices; filled in with zeros...\n");
     }
-    */
 
+    // allocating space for 2d matrix
     int **matrixOne = (int**)malloc(sizeof(int*)*row);
     int **matrixTwo = (int**)malloc(sizeof(int*)*row);
     
-    for(int i=0; i<row; i++){
+    for(int i = 0; i < row; i++){
         matrixOne[i]=(int*)malloc(sizeof(int)*col);
         matrixTwo[i]=(int*)malloc(sizeof(int)*col);
     }
 
     printf("Rows: %d \n", row);
     printf("Columns: %d \n", col);
-    int index = 3;
+    int index = 3; // since 1 and 2 are rows and cols
 
-
-    for(int j = 0; j< row; j++){
-        for(int i = 0; i< col; i++){
-            if(argv[index]!= NULL){
+    // fill in Matrix 1
+    for(int j = 0; j < row; j++){
+        for(int i = 0; i < col; i++){
+            if(argv[index] != NULL){
                 matrixOne[j][i]= atoi(argv[index]);
                 index++;
             } else {
@@ -43,12 +40,11 @@ int main(int argc, char **argv){
             }
         }
     }
-    //index++;
 
-    // take in entries
-    for(int j = 0; j< row; j++){
-        for(int i = 0; i< col; i++){
-            if(argv[index]!= NULL){
+    // fill in Matrix 2
+    for(int j = 0; j < row; j++){
+        for(int i = 0; i < col; i++){
+            if(argv[index] != NULL){
                 matrixTwo[j][i]=atoi(argv[index]);
                 index++;
             } else {
@@ -59,8 +55,8 @@ int main(int argc, char **argv){
 
     // print matrix 1
     printf("Matrix 1: \n");
-    for(int j=0; j< row; j++){
-        for(int i=0; i< col; i++){
+    for(int j = 0; j < row; j++){
+        for(int i = 0; i < col; i++){
             printf("%d ", matrixOne[j][i]);
         }
         printf("\n");
@@ -68,20 +64,17 @@ int main(int argc, char **argv){
 
     // print matrix 2
     printf("Matrix 2: \n");
-    for(int j=0; j< row; j++){
-        for(int i=0; i< col; i++){
+    for(int j = 0; j < row; j++){
+        for(int i = 0; i < col; i++){
             printf("%d ", matrixTwo[j][i]);
         }
         printf("\n");
     }    
-
-    //add 
     
-    //subtract 
+    // TODO: Implement functions to add, subtract, multiply these matrices. Then output their result. 
+    
 
-    //multiply
-
-    for(int i=0; i<row; i++){
+    for(int i = 0; i < row; i++){
         free(matrixOne[i]);
         free(matrixTwo[i]);
     }
