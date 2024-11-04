32d46f3637a9fec3d27db598ef9b5e8dbc7c0638
Rachelle Gaudreau <107381065+rachellegaudreau@users.noreply.github.com>
2024-11-04 14:38:56 -0500

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// TODO: Implement functions to add, subtract, multiply these matrices. Then output their result. 
+void printMatrix(int** matrix, int row, int col){
+    for(int j = 0; j < row; j++){
+        for(int i = 0; i < col; i++){
+            printf("%d ", matrix[j][i]);
+        }
+        printf("\n");
+    }
+}
+int** addMatrix(int ** num1, int ** num2, int row, int col){
+    int **result = (int**)malloc(sizeof(int*)*row);
+    for(int i = 0; i < row; i++){
+        result[i] = (int*)malloc(sizeof(int)*col);
+    }
+    for(int j=0; j<row;j++){
+        for(int i=0; i<col;i++){
+            result[j][i]=num1[j][i]+num2[j][i];
+        }
+    }
+    return result;
+}
+
+int** subtractMatrix(int** num1, int** num2, int row, int col){
+    int **result = (int**)malloc(sizeof(int*)*row);
+    for(int i = 0; i < row; i++){
+        result[i] = (int*)malloc(sizeof(int)*col);
+    }
+    for(int j=0; j<row;j++){
+        for(int i=0; i<col;i++){
+            result[j][i]=num1[j][i]-num2[j][i];
+        }
+    }
+    return result;
+}
+
+int** multMatrix(int** num1, int** num2, int row, int col){
+    if(row != col){
+        printf("Columns and Rows do not match!!\n");
+        return;
+    }
+    int **result = (int**)malloc(sizeof(int*)*row);
+    for(int i = 0; i < row; i++){
+        result[i] = (int*)malloc(sizeof(int)*col);
+    }
+    for(int j=0; j<row;j++){
+        for(int i=0; i<col;i++){
+            result[j][i]= result[j][i] + (num1[j][i] * num2[j][i]);
+        }
+    }
+    return result;
+}
+
 int main(int argc, char **argv){
     if(argc<3){
         printf("How to use: <rows> <columns> <matrix_entries> \n");
@@ -55,24 +107,18 @@ int main(int argc, char **argv){
 
     // print matrix 1
     printf("Matrix 1: \n");
-    for(int j = 0; j < row; j++){
-        for(int i = 0; i < col; i++){
-            printf("%d ", matrixOne[j][i]);
-        }
-        printf("\n");
-    }
+    printMatrix(matrixOne, row, col);
 
     // print matrix 2
     printf("Matrix 2: \n");
-    for(int j = 0; j < row; j++){
-        for(int i = 0; i < col; i++){
-            printf("%d ", matrixTwo[j][i]);
-        }
-        printf("\n");
-    }    
+    printMatrix(matrixTwo, row, col);  
     
-    // TODO: Implement functions to add, subtract, multiply these matrices. Then output their result. 
-    
+    printf("Matrix Sum: \n");
+    printMatrix(addMatrix(matrixOne,matrixTwo,row,col), row, col);
+    printf("Matrix Difference: \n");
+    printMatrix(subtractMatrix(matrixOne,matrixTwo,row,col),row,col);
+    printf("Matrix Product: \n");
+    printMatrix(multMatrix(matrixOne,matrixTwo,row,col), row, col); 
 
     for(int i = 0; i < row; i++){
         free(matrixOne[i]);
